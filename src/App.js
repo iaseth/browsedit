2f78b7d6a15aa7f2a808e4ddfe44347321a1eec1
Ankur Seth <iankurseth@gmail.com>
2022-02-27 08:25:44 +0530

    Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="min-h-screen bg-purple-400 text-white flex">
+				<h2 className="text-4xl m-auto">Hallo <span className="bg-purple-600 px-2 py-1 rounded">PWA</span></h2>
+			</div>
+		</div>
+	);
 }
 
 export default App;
