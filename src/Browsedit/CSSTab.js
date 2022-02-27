ade24c3164ec570c4b640e2db47c0327493b0798
Ankur Seth <iankurseth@gmail.com>
2022-02-27 15:46:16 +0530

    Separated HTML and CSS textarea elements

--- src/Browsedit/CSSTab.js
+++ src/Browsedit/CSSTab.js
@@ -1,5 +1,7 @@
+import React from 'react';
+
 import {Heading} from './Utils';
-import Textarea from './Textarea';
+import LineNumbers from './LineNumbers';
 
 
 
@@ -7,10 +9,47 @@ export default function CSSTab ({
 	cssText, setCssText
 }) {
 
+	const tref = React.useRef(null);
+
+	function handleKeyDown (e) {
+		const keyCode = e.keyCode;
+		const target = e.target;
+
+		if (keyCode === 9) {
+			// tab key
+			e.preventDefault();
+			const start = target.selectionStart;
+			const end = target.selectionEnd;
+			let selected = cssText.slice(start, end);
+			if (!selected) {
+				let index = start;
+				while (index > 0) {
+					let c = cssText[index-1];
+					if (c === '\n' || c === ' ') break;
+					index--;
+				}
+				selected = cssText.slice(index, start);
+			}
+
+			// let newText = cssText.slice(0, start-selected.length) + `<${selected}></${selected}>` + cssText.slice(start);
+			// setCssText(cssText => newText);
+		} else if (keyCode === 13) {
+			// enter key
+		}
+		// console.log("Key: " + keyCode);
+	}
+
+	function handleChange (e) {
+		setCssText(e.target.value);
+	}
+
 	return (
 		<div className="CSSTab basis-0 grow p-4 pt-2 bg-slate-200 flex flex-col">
 			<Heading>CSS</Heading>
-			<Textarea text={cssText} setText={setCssText} />
+			<div className="Textarea grow flex">
+				<LineNumbers text={cssText} />
+				<textarea ref={tref} value={cssText} onKeyDown={handleKeyDown} onChange={handleChange} className="textarea w-full px-2 py-2 outline-none resize-none"></textarea>
+			</div>
 		</div>
 	);
 }
--- src/Browsedit/HTMLTab.js
+++ src/Browsedit/HTMLTab.js
@@ -1,5 +1,7 @@
+import React from 'react';
+
 import {Heading} from './Utils';
-import Textarea from './Textarea';
+import LineNumbers from './LineNumbers';
 
 
 
@@ -7,10 +9,47 @@ export default function HTMLTab ({
 	htmlText, setHtmlText
 }) {
 
+	const tref = React.useRef(null);
+
+	function handleKeyDown (e) {
+		const keyCode = e.keyCode;
+		const target = e.target;
+
+		if (keyCode === 9) {
+			// tab key
+			e.preventDefault();
+			const start = target.selectionStart;
+			const end = target.selectionEnd;
+			let selected = htmlText.slice(start, end);
+			if (!selected) {
+				let index = start;
+				while (index > 0) {
+					let c = htmlText[index-1];
+					if (c === '\n' || c === ' ') break;
+					index--;
+				}
+				selected = htmlText.slice(index, start);
+			}
+
+			let newText = htmlText.slice(0, start-selected.length) + `<${selected}></${selected}>` + htmlText.slice(start);
+			setHtmlText(htmlText => newText);
+		} else if (keyCode === 13) {
+			// enter key
+		}
+		// console.log("Key: " + keyCode);
+	}
+
+	function handleChange (e) {
+		setHtmlText(e.target.value);
+	}
+
 	return (
 		<div className="HTMLTab basis-0 grow p-4 pt-2 flex flex-col">
 			<Heading>HTML</Heading>
-			<Textarea text={htmlText} setText={setHtmlText} />
+			<div className="Textarea grow flex">
+				<LineNumbers text={htmlText} />
+				<textarea ref={tref} value={htmlText} onKeyDown={handleKeyDown} onChange={handleChange} className="textarea w-full px-2 py-2 outline-none resize-none"></textarea>
+			</div>
 		</div>
 	);
 }
