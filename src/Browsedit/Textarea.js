e4a4652b3081acff071518ed25f30f3ce9fc7cbb
Ankur Seth <iankurseth@gmail.com>
2022-02-27 13:23:17 +0530

    Ignoring tab for now

--- src/Browsedit/Textarea.js
+++ src/Browsedit/Textarea.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import LineNumbers from './LineNumbers';
 
 
@@ -5,12 +7,28 @@ import LineNumbers from './LineNumbers';
 export default function Textarea ({
 	text, setText
 }) {
-	//
+
+	const tref = React.useRef(null);
+
+	function handleKeyDown (e) {
+		const keyCode = e.keyCode;
+		if (keyCode === 9) {
+			// tab key
+			e.preventDefault();
+		} else if (keyCode === 13) {
+			// enter key
+		}
+		// console.log("Key: " + keyCode);
+	}
+
+	function handleChange (e) {
+		setText(e.target.value);
+	}
 
 	return (
 		<div className="Textarea grow flex">
 			<LineNumbers text={text} />
-			<textarea value={text} onChange={(e) => setText(e.target.value)} className="textarea w-full px-2 py-2 outline-none resize-none"></textarea>
+			<textarea ref={tref} value={text} onKeyDown={handleKeyDown} onChange={handleChange} className="textarea w-full px-2 py-2 outline-none resize-none"></textarea>
 		</div>
 	);
 }
