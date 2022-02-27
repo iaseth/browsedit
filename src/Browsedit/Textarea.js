90a47819ef53975b8f7236ed2e2bbaabc595270c
Ankur Seth <iankurseth@gmail.com>
2022-02-27 15:35:35 +0530

    Enabled tab completion for HTML

--- src/Browsedit/Textarea.js
+++ src/Browsedit/Textarea.js
@@ -12,9 +12,26 @@ export default function Textarea ({
 
 	function handleKeyDown (e) {
 		const keyCode = e.keyCode;
+		const target = e.target;
+
 		if (keyCode === 9) {
 			// tab key
 			e.preventDefault();
+			const start = target.selectionStart;
+			const end = target.selectionEnd;
+			let selected = text.slice(start, end);
+			if (!selected) {
+				let index = start;
+				while (index > 0) {
+					let c = text[index-1];
+					if (c === '\n' || c === ' ') break;
+					index--;
+				}
+				selected = text.slice(index, start);
+			}
+
+			let newText = text.slice(0, start-selected.length) + `<${selected}></${selected}>` + text.slice(start);
+			setText(text => newText);
 		} else if (keyCode === 13) {
 			// enter key
 		}
